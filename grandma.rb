5761252a8a6844a27fba37342100cfbdd5334b17
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 17:23:47 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,28 @@
-# Write a speak_to_grandma method.
+def speak_to_grandma(phrase)
+  if "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!".upcase
+    "I LOVE YOU TOO PUMPKIN!"
+  elsif "Hi Nana, how are you?" == "Hi Nana, how are you?".upcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif "Hi!" == "Hi!".upcase
+    "HUH?! SPEAK UP, SONNY!"
+  elsif "WHAT DID YOU EAT TODAY?" == "WHAT DID YOU EAT TODAY?".upcase
+    "NO, NOT SINCE 1938!"
+  elsif "WHAT?" == "WHAT?".upcase
+    "NO, NOT SINCE 1938!"
+  elsif "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!".upcase
+    "I LOVE YOU TOO PUMPKIN!"
+  else "Hi Nana, how are you?" == "Hi Nana, how are you?".upcase
+    "HUH?! SPEAK UP, SONNY!"
+  end
+
+
+    # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
