f4323bd894595919fa0c065c65f1c98c2a67222e
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 17:36:12 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -11,7 +11,7 @@ def speak_to_grandma(phrase)
   		return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.downcase
       return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.upcase
+    elsif phrase == phrase
       return "NO, NOT SINCE 1938!"
     # NO, NOT SINCE 1938!
     else
