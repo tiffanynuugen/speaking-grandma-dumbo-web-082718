6519615e8522ebd4e139dac214f8c5541ed7b51c
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-14 17:32:34 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -7,11 +7,11 @@ def speak_to_grandma(phrase)
       return "I LOVE YOU TOO PUMPKIN!"
     # If you shout, she can hear you (or at least she thinks so)
     # and yells back
-    elsif phrase == phrase.downcase
+  elsif phrase == phrase.upcase
   		return "HUH?! SPEAK UP, SONNY!"
-    elsif phrase == phrase.downcase
-      return "HUH?! SPEAK UP, SONNY!"
     elsif phrase == phrase.upcase
+      return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == phrase.downcase
       return "NO, NOT SINCE 1938!"
     # NO, NOT SINCE 1938!
     else
